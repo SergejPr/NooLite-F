98576864703073fd26958af2df1ac7742a35421d
Sergey <sergej.prytkov@gmail.com>
2017-10-17 01:42:12 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from distutils.core import setup
+from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+with open(path.join(here, 'LICENSE.txt'), encoding='utf-8') as f:
+    license_description = f.read()
 
 setup(
     name='NooLiteF',
     packages=['NooLiteF'],
     version='0.0.3',
-    license=open('LICENSE.txt').read(),
-    description='Module to work with NooLiteF (MRTF-64-USB)',
-    long_description=open('README.rst').read(),
+    license=license_description,
+    description='Module to work with NooLiteF (MTRF-64-USB)',
+    long_description=long_description,
     author='Sergey Prytkov',
     author_email='sergej.prytkov@gmail.com',
     url='https://github.com/SergejPr/NooLite-F',
