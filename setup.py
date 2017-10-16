938524cf9b60f41e9bd083cea5c83d6c9d589db6
Sergey <sergej.prytkov@gmail.com>
2017-10-17 02:41:49 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
 from setuptools import setup
-
 import io
-
 
 with io.open('README.rst', encoding="utf-8") as readme_file:
     long_description = readme_file.read()
 
-with io.open('LICENSE.txt', encoding="utf-8") as license_file:
-    license_description = license_file.read()
-
 setup(
     name="NooLite_F",
     packages=["NooLite_F"],
-    version="0.0.1",
-    license=license_description,
+    version="0.0.2",
+    license="MIT License",
     description="Module to work with NooLite_F (MTRF-64-USB)",
     long_description=long_description,
     author="Sergey Prytkov",
     author_email="sergej.prytkov@gmail.com",
-    url="https://github.com/SergejPr/NooLite_F",
+    url="https://github.com/SergejPr/NooLite-F",
     keywords="noolite noolite-f noolitef",
     install_requires=["pyserial"],
     platforms="osx, posix, linux, windows",
