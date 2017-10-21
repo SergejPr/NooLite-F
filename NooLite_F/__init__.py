71ce2f13235232df4ff9cef8890e75afc351b8a3
Sergey <sergej.prytkov@gmail.com>
2017-10-21 22:58:45 +0300

    refactor project structure

--- NooLite_F/__init__.py
+++ NooLite_F/__init__.py
@@ -1,13 +1,4 @@
-from NooLite_F import Action
-from NooLite_F import Command
-from NooLite_F import Mode
-from NooLite_F import ModuleState
-from NooLite_F import ModuleMode
-from NooLite_F import ModuleInfo
-from NooLite_F import Request
-from NooLite_F import Response
-from NooLite_F import ResponseCode
-
+from .NooLite_F import NooLiteF
 
 
 
