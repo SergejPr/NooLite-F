bea5fa402f9034fe1c7e07292a23c1e04fbed3f9
Sergey <sergej.prytkov@gmail.com>
2018-10-13 18:54:11 +0300

    change log format message

--- NooLite_F/MTRF64/MTRF64USBAdapter.py
+++ NooLite_F/MTRF64/MTRF64USBAdapter.py
@@ -105,7 +105,7 @@ class IncomingData(object):
 _LOGGER = logging.getLogger("MTRF64USBAdapter")
 _LOGGER.setLevel(logging.DEBUG)
 _LOGGER_HANDLER = logging.StreamHandler()
-_LOGGER_HANDLER.setFormatter(logging.Formatter("%(asctime)s;%(levelname)s;%(message)s", "%Y-%m-%d %H:%M:%S"))
+_LOGGER_HANDLER.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", "%Y-%m-%d %H:%M:%S"))
 _LOGGER.addHandler(_LOGGER_HANDLER)
 
 
