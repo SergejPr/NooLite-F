3e8da033b79b3578e121c458178986c2006df005
macros0 <20460562+macros0@users.noreply.github.com>
2019-02-21 10:51:48 +0300

    Bug fix after HA upgrade to version 0.88.0
    Presumably due to Python 3.7

--- NooLite_F/__init__.py
+++ NooLite_F/__init__.py
@@ -1,6 +1,5 @@
 from NooLite_F.NooLiteFController import NooLiteFController, Direction, NooLiteFListener, BatteryState, ModuleMode
-from NooLite_F.NooLiteFController import ModuleInfo, ModuleBaseStateInfo, ModuleExtraStateInfo, ModuleChannelsStateInfo, ModuleState, ServiceModeState, InputMode, \
-    DimmerCorrectionConfig, ModuleConfig, NooliteModeState
+from NooLite_F.NooLiteFController import ModuleInfo, ModuleBaseStateInfo, ModuleExtraStateInfo, ModuleChannelsStateInfo, ModuleState, ServiceModeState, InputMode, DimmerCorrectionConfig, ModuleConfig, NooliteModeState
 from NooLite_F.NooLiteFController import ResponseBaseInfo, ResponseExtraInfo, ResponseChannelsInfo, ResponseModuleConfig, ResponseDimmerCorrectionConfig
 from NooLite_F.Modules import Switch, ExtendedSwitch, Dimmer, RGBLed
 from NooLite_F.Sensors import GenericListener, TempHumiSensor, MotionSensor, RemoteController, RGBRemoteController
